c2ae90b2ea6a77a95c91399b0524fd28f8125d81
Julien Bisagni <julienbisagni@gmail.com>
2021-01-23 23:29:37 +0100

    Fixed wrong path src/images error

--- .cache/caches/gatsby-plugin-typography/typography.js
+++ .cache/caches/gatsby-plugin-typography/typography.js
@@ -1,1 +1,1 @@
-module.exports = require("/Users/julien/code/JulienBisagni/gatsby-boilerplate/src/utils/typography")
+module.exports = require("/Users/julien/code/JulienBisagni/foot-dimanche/src/utils/typography")
